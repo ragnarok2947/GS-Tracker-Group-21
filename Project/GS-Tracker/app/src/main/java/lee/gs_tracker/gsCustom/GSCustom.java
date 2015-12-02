5673408593edc9500cdd9fd12d22f7441f4c3697
joerpaul <joerpaul@hotmail.com>
2015-12-01 23:06:03 -0500

    fixed back button crash on GSCustom, added landscape layout for sports_add_game

--- Project/GS-Tracker/app/src/main/java/lee/gs_tracker/gsCustom/GSCustom.java
+++ Project/GS-Tracker/app/src/main/java/lee/gs_tracker/gsCustom/GSCustom.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -123,7 +124,10 @@ public class GSCustom extends AppCompatActivity implements QuickQuestionDialog.L
       // as you specify a parent activity in AndroidManifest.xml.
       int id = item.getItemId();
 
-      //noinspection SimplifiableIfStatement
+      if (id == android.R.id.home)
+      {
+         NavUtils.navigateUpFromSameTask(this);
+      }
       if (id == R.id.gamelist_deleteall)
       {
          // display confirmation, "deleteall" handled by onQuickQuestionDialogClick further down
